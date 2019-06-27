e22de51172750d7d919934601ba671a8c1c2f06c
unknown <samuel_newhouse@yahoo.com>
2019-06-27 11:51:35 -0500

    Rearrange works and add to description

--- scripts/index.js
+++ scripts/index.js
@@ -3,59 +3,58 @@ document.addEventListener("DOMContentLoaded", () => {
   const works = [
     {
       title: "Fox Creek Cedar Oil",
-      description: "A real-world business website for a family-owned operation.",
+      description: "A real-world business website for a family-owned operation. For this site in particular, I extensively researched and applied proper SEO techniques.",
       overRideURL: "https://www.foxcreekcedaroil.com"
     },
     {
-      title: "Wikipedia Quick Search",
-      description: "Search wikipedia and see a summary of the top 10 results. Optionally search again by any of the returned titles.",
-      gitHubName: "wikipedia-viewer"
-    },
-    {
       title: "Drum Machine",
-      description: "A drum machine with recording, playback, and editing features.",
+      description: "A drum machine with recording, playback, and editing features. This is my most complicated project, involving a heavy use of react, redux, and redux thunks.",
       gitHubName: "drum-machine"
     },
     {
+      title: "Wikipedia Quick Search",
+      description: "Search wikipedia and see a summary of the top 10 results. Optionally search again by any of the returned titles. Very fast, and even fun, for quickly seeing how some topics are related on Wikipedia.",
+      gitHubName: "wikipedia-viewer"
+    },
+    {
       title: "Markdown Previewer",
-      description: "A markdown previewer with the ability to minimize and maximize the editor and viewer at any resolution.",
+      description: "A markdown previewer with the ability to minimize and maximize the editor and viewer at any resolution. This was an especially interesting challenge in terms of making it fully responsive.",
       gitHubName: "markdown-previewer"
     },
     {
       title: "Random Wikiquote API",
-      description: "A JavaScript API for getting a random quote from a random pages on en.wikiquote.org",
+      description: "A JavaScript API for getting a random quote from a random pages on en.wikiquote.org. This is suprisingly difficult to do because the wikiquote API doesn't support querying for random quotes.",
       gitHubName: "random-wikiquote",
       sourceOnly: true
     },
     {
       title: "Random Wikiquotes",
-      description: "Uses the aforementioned Random Wikiquote API to display random quotes.",
+      description: "Uses the aforementioned Random Wikiquote API to display random quotes. Generated quotes can be tweeted or Google searched with the press of a button.",
       gitHubName: "quote-machine2",
     },
     {
-      title: "Premium Primes",
-      description: "A mock product landing page.",
-      gitHubName: "product-page",
-    },
-    {
-      title: "Epictetus",
-      description: "A tribute page to Epictetus.",
-      gitHubName: "epictetus",
-    },
-    {
       title: "Responsive Diagonally Distributed Elements",
-      description: "A JavaScript solution for responsively aligning elements on a diagonal line.",
-      gitHubName: "responsive-diagonally-distributed-elements",
+      description: 'A JavaScript solution for responsively aligning elements on a diagonal line. This is a much more comprehensive solution than manually coding ratios in flexboxes.',
       sourceOnly: true
     },
     {
       title: "RDDE Technical Documentation",
-      description: "My explanation and documentation for Responsive Diagonally Distributed Elements.",
+      description: 'My explanation and documentation for Responsive Diagonally Distributed Elements. Find the "Introduction" and "Compare Blocking Cases" sections to see examples of RDDE in action.',
       gitHubName: "rdde-tech-doc",
     },
     {
+      title: "Epictetus",
+      description: "A tribute page to Epictetus. Researching the content for this page was the most time consuming part, but it was worth it.",
+      gitHubName: "epictetus",
+    },
+    {
+      title: "Premium Primes",
+      description: "A straighforward mock product landing page. It's simple, but I did learn how to make embeded YouTube videos fully responsive.",
+      gitHubName: "product-page",
+    },
+    {
       title: "Survey",
-      description: "A mock survey page.",
+      description: "A basic mock survey page. It was a useful excercise for learning about Bootstrap form features.",
       gitHubName: "survey",
     },
   ];
